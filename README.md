8c36b3ed6241cbf63406a422c50c027d92536620
sergiokb <sergiokb@yandex.ru>
2022-12-10 01:13:54 +0300

    README draft

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # LR-1-algorithm
+
+### About running
+#### For your own test  :
+Run main.cpp, having your grammar in *grammar.txt* and input words in *words.txt*
+
+Format of grammar.txt:
+```
+Starting with: S
+1. S -> SaSb
+2. S -> $
+```
+
+Format of words.txt:
+```
+abba
+aabb
+```
+
+#### For checking my tests:
+
+Run my_tests.cpp
--- main.cpp
+++ main.cpp
@@ -3,10 +3,8 @@
 int main() {
     std::vector<std::string> words;
     std::vector<std::string> ans;
-    solution("tests/examples/grammar_0.txt",
-             "tests/examples/words_0.txt", words, ans);
-//    solution("grammar.txt",
-//         "words.txt", words, ans);
+    solution("grammar.txt",
+         "words.txt", words, ans);
     for(size_t i = 0; i < ans.size(); ++i) {
         std::cout << words[i] << " - " << ans[i] << '\n';
     }
